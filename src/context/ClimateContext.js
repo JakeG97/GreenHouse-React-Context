2e5cb8493bb8f72272608eace7b6cfb55fdf3211
Danielle Lei <Danielle.lei2014@gmail.com>
2023-03-02 14:13:57 -0800

    phase4 done

--- src/context/ClimateContext.js
+++ src/context/ClimateContext.js
@@ -1,9 +1,9 @@
 // Temperature has a default value of 50 degrees
 // Humidity has a default value of 40%
-import { createContext, useState } from 'react';
+import { createContext, useState, useContext } from 'react';
 
 export const ClimateContext = createContext();
-
+export const useClimate = () => useContext(ClimateContext)
 export default function ClimateProvider({ children }) {
     const [temperature, setTemperature] = useState(50);
 
